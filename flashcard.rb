da3dad58c5ff9954aa9fb451d139a9f8ecc34760
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-16 11:11:27 -0500

    defined classes

--- flashcard.rb
+++ flashcard.rb
@@ -1,1 +1,38 @@
 # Main file for 
+module Flashcards
+  
+  class Card
+    attr_reader :term, :definition
+
+    def intialize(term, definition)
+      @term = term
+      @definition = definition
+    end
+
+
+  end
+
+  class Deck
+    attr_accessor :file 
+    
+    def initialize(file = 'flashcard_sample.txt')
+      @file = File.open(file)
+    end
+    
+    def parse
+      file.read
+    end
+
+  end
+
+
+  class Controller
+
+  end
+
+  class View
+  end
+
+end
+
+d = Deck
